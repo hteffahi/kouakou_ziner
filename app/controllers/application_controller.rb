a3f9704f20886c135a267100ac8931741f0cbeaf
Flavien Leray <lerayflavien@gmail.com>
2023-11-29 17:11:49 +0100

    dashboard after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,8 @@ class ApplicationController < ActionController::Base
 
     devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name])
   end
+  
+  def after_sign_in_path_for(resource)
+    dashboard_path
+  end
 end
