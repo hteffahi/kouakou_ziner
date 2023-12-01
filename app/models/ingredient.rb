41e44278796411d2ec67365e781a194963029321
Flavien Leray <lerayflavien@gmail.com>
2023-12-01 14:29:50 +0100

    manque user friendly de la date

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,7 +1,14 @@
 class Ingredient < ApplicationRecord
   belongs_to :user
+
+  VALID_UNITS = %w(tsp tbsp cup oz lb t pc)
   validates :name, presence: true
   validates :amount, presence: true, numericality: { greater_than: 0 }
+  validates :unit, inclusion: { in: VALID_UNITS }
+
+  def self.valid_units
+    VALID_UNITS
+  end
 
   include PgSearch::Model
   pg_search_scope :search_by_name,
